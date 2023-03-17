819624484b89d5b1adb042431ad2874c24632fb9
Sumayea Rahman <shorbonam104@gmail.com>
2023-03-17 21:19:04 +0600

    value banan

---
+++ js/deposit.js
@@ -0,0 +1,9 @@
+// step-1 add event listener to the deposit button
+document.getElementById('btn-deposit').addEventListener('click',function(){
+    // step-2 get the  deposit amount from the deposit input  field
+    // console.log('clicked')
+    const depositField = document.getElementById('deposit-input');
+    const depositAmount = depositField.value;
+    // value banan e vul korle hobe na
+    // console.log(depositAmount);
+})
